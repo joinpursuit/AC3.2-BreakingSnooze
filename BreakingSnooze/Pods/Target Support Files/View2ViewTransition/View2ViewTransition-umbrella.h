136596a4c4056ad64b6cd2a862dd5bcbac21f456
Liam <liamdkane@gmail.com>
2017-01-10 12:28:54 -0500

    cocoa pods

---
+++ BreakingSnooze/Pods/Target Support Files/View2ViewTransition/View2ViewTransition-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double View2ViewTransitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char View2ViewTransitionVersionString[];
+
