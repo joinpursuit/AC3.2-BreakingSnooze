3a9fe6191f18f810ee90131a500ebd8ed6a2f87d
Cristopher Chavez <cristopherchavezny@gmail.com>
2017-01-10 18:20:54 -0500

    CollectionView in Tableview cell working but still crasing

--- BreakingSnooze/BreakingSnooze/CategoryTableViewCell.swift
+++ BreakingSnooze/BreakingSnooze/CategoryTableViewCell.swift
@@ -7,28 +7,86 @@
 //
 
 import UIKit
+import CoreData
 
-class CategoryTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource {
+fileprivate let collectionViewReusableID = "SourceCollectionViewIdentifier"
 
+class CategoryTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, NSFetchedResultsControllerDelegate {
+    
+    var category: String! {
+        
+        didSet {
+            initializeFetchedResultsController()
+        }
+        
+    }
+    
+    
     @IBOutlet weak var sourceCollectionView: UICollectionView!
     
+    var mainContext: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    private var controller: NSFetchedResultsController<NewsSource>!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         sourceCollectionView.delegate = self
         sourceCollectionView.dataSource = self
+        
+    }
+    
+    func initializeFetchedResultsController() {
+        let request: NSFetchRequest<NewsSource> = NewsSource.fetchRequest()
+        let sort = NSSortDescriptor(key: "sourceLogo", ascending: true)
+        
+        request.sortDescriptors =  [sort]
+        
+        let predicate = NSPredicate(format: "%K == %@", "category", self.category)
+        request.predicate = predicate
+        self.controller = NSFetchedResultsController(fetchRequest: request, managedObjectContext: mainContext, sectionNameKeyPath: "category", cacheName: nil)
+        controller.delegate = self
+        
+        do {
+            try controller.performFetch()
+            // self.tableView.reloadData()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
     }
 
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        <#code#>
+        guard let sections = controller.sections else {
+            print("No sections in fetchedResultsController")
+            return 0
+        }
+        return sections.count
     }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        guard let sections = controller.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: collectionViewReusableID, for: indexPath)
+        if let cell = cell as? SourceCollectionViewCell {
+            let source = controller.object(at: indexPath)
+            APIManager.shared.getData(urlString: source.sourceLogo!, completion: { (data) in
+                guard let validData = data else { return }
+                guard let validImage = UIImage(data: validData) else { return }
+                cell.sourceImageView.image = validImage
+            })
+        }
+        
+        return cell
     }
 
 }
