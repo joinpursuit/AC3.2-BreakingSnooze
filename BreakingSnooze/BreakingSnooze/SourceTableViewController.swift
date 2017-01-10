c66759352c3f69562bf06c49c4dce8dbf42d9c18
Cristopher Chavez <cristopherchavezny@gmail.com>
2017-01-10 15:21:46 -0500

    TableViewController fiole added

---
+++ BreakingSnooze/BreakingSnooze/SourceTableViewController.swift
@@ -0,0 +1,117 @@
+//
+//  SourceTableViewController.swift
+//  BreakingSnooze
+//
+//  Created by Cris on 1/10/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class SourceTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    var mainContext: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+    
+    private var controller: NSFetchedResultsController<NewsSource>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initializeFetchedResultsController()
+    }
+    
+    func initializeFetchedResultsController() {
+        let request: NSFetchRequest<NewsSource> = NewsSource.fetchRequest()
+        let sort = NSSortDescriptor(key: "category", ascending: true)
+        request.sortDescriptors = [sort]
+        
+        self.controller = NSFetchedResultsController(fetchRequest: request, managedObjectContext: mainContext, sectionNameKeyPath: nil, cacheName: nil)
+        controller.delegate = self
+        
+        do {
+            try controller.performFetch()
+            // self.tableView.reloadData()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        guard let sections = controller.sections else {
+            print("No sections in fetchedResultsController")
+            return 0
+        }
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        // Configure the cell...
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let category = controller.object(at: IndexPath(item: 1, section: section))
+        return category.category
+    }
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
