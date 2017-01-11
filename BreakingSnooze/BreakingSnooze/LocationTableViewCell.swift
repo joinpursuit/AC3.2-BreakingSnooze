679e69a62ec8945ecc659cb4e6b5459e326e46e5
Cristopher Chavez <cristopherchavezny@gmail.com>
2017-01-11 13:19:46 -0500

    Made SettingsTableView

---
+++ BreakingSnooze/BreakingSnooze/LocationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LocationTableViewCell.swift
+//  BreakingSnooze
+//
+//  Created by Cris on 1/11/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BreakingSnooze/BreakingSnooze/RadioStationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RadioStationTableViewCell.swift
+//  BreakingSnooze
+//
+//  Created by Cris on 1/11/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class RadioStationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
